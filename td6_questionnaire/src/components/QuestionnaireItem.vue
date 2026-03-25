ee98febeb2d7a9188dcc34bab752a38780c27049
korasrar <celestin.maubert@etu.univ-orleans.fr>
2026-03-25 14:12:57 +0100

    feat: questionnaire detail + edit, etc...

--- td6_questionnaire/src/components/QuestionnaireItem.vue
+++ td6_questionnaire/src/components/QuestionnaireItem.vue
@@ -3,20 +3,10 @@ const props = defineProps({
   questionnaire: Object,
 });
 
-const emit = defineEmits(["delete", "update"]);
+const emit = defineEmits(["delete"]);
 
 function deleteQuestionnaire() {
   emit("delete", props.questionnaire.id);
-}
-
-function updateQuestionnaire() {
-  const newNom = prompt("Modifier le questionnaire", props.questionnaire.titre_questionnaire);
-  if (newNom) {
-    emit("update", {
-      questionnaire: props.questionnaire,
-      newNomquestionnaire: newNom,
-    });
-  }
 }
 </script>
 
@@ -26,13 +16,10 @@ function updateQuestionnaire() {
       <div class="d-flex justify-content-between align-items-center mb-3">
         <h3 class="card-title mb-0">{{ questionnaire.title }}</h3>
         <div>
-          <button class="btn btn-warning btn-sm me-2" @click="updateQuestionnaire">Modifier</button>
+          <router-link :to="`/questionnaires/${questionnaire.id}`" class="btn btn-warning btn-sm me-2">Modifier</router-link>
           <button class="btn btn-danger btn-sm" @click="deleteQuestionnaire">Supprimer</button>
         </div>
       </div>
-      
-      <h4>Les questions</h4>
-      <!-- rien d'autre pour le moment (questions item dans un second temps) -->
     </div>
   </div>
 </template>
